64848e6eda8be27c15c153e096fb918f2f9d8983
Andriy Redko <andriy.redko@aiven.io>
2023-04-12 00:36:09 +0800

    [FLINK-31696] ElasticSearch nightly CI failure
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- flink-connector-elasticsearch6/src/test/java/org/apache/flink/streaming/connectors/elasticsearch/table/Elasticsearch6DynamicSinkITCase.java
+++ flink-connector-elasticsearch6/src/test/java/org/apache/flink/streaming/connectors/elasticsearch/table/Elasticsearch6DynamicSinkITCase.java
@@ -61,6 +61,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.apache.flink.streaming.connectors.elasticsearch.table.TestContext.context;
 import static org.apache.flink.table.api.Expressions.row;
@@ -397,6 +398,10 @@ public class Elasticsearch6DynamicSinkITCase extends TestLogger {
         public DynamicTableSink.DataStructureConverter createDataStructureConverter(
                 DataType consumedDataType) {
             return null;
+        }
+
+        public Optional<int[][]> getTargetColumns() {
+            return Optional.empty();
         }
     }
 }
